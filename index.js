30784983fcbbe8f0466e200ba37509fd315323a6
ChandanKumar653 <chandankumar6204995@gmail.com>
2023-04-24 06:28:54 +0530

    Testing Post Api

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
+app.use(cors())
+const bodyParser = require('body-parser')
+app.use(bodyParser.urlencoded({ extended: true }));
 app.get('/test', (req, res) => {
-    res.send("Live")
+    res.sendFile(__dirname + "/index.html")
+})
+app.post('/login', (req, res) => {
+    const email = req.body.email;
+    const password = req.body.password;
+    console.log(req.body)
+    res.json(req.body)
 })
 app.listen(port, () => {
     console.log("servver is running at port " + port)
