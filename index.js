b91f518acb77839d08346b188a058c8f356d56b1
Chandan Kumar <73373011+ChandanKumar653@users.noreply.github.com>
2023-04-24 08:37:24 +0530

    Update index.js

--- index.js
+++ index.js
@@ -6,15 +6,12 @@ const port = process.env.PORT || 3000
     // }));
 const bodyParser = require('body-parser')
 app.use(bodyParser.urlencoded({ extended: true }));
-app.get('/test', (req, res) => {
-    res.sendFile(__dirname + "/index.html")
-})
 app.post('/login', (req, res) => {
     const email = req.body.email;
     const password = req.body.password;
     console.log(req.body)
     console.log(email+password)
-    res.json(req.body)
+  res.send(email + "  " + password)
 })
 app.listen(port, () => {
     console.log("servver is running at port " + port)
