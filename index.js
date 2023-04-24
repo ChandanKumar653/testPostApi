372e6f430a2811d53c7229ef76ca1f86e44b834d
ChandanKumar653 <chandankumar6204995@gmail.com>
2023-04-24 07:38:15 +0530

    test

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
-app.use(cors())
+    // app.use(cors({
+    //     origin: '*'
+    // }));
 const bodyParser = require('body-parser')
 app.use(bodyParser.urlencoded({ extended: true }));
 app.get('/test', (req, res) => {
